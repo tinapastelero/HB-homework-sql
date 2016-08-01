bfc855a59a52704cc89b88e2d1ed798bcfce22c0
Tina Pastelero <tinapastelero@gmail.com>
2016-08-01 02:17:33 +0000

    completed more practice

--- more-practice.sql
+++ more-practice.sql
@@ -4,8 +4,21 @@
 
 -- INSERT
 
+INSERT INTO models (year, brand_name, name) VALUES ('2015','Chevrolet','Malibu');
+INSERT INTO models (year, brand_name, name) VALUES ('2015','Subaru','Outback');
+
+
 
 -- CREATE TABLE
 
+CREATE TABLE awards(
+    name VARCHAR(30),
+    year INTEGER,
+    winner_id INTEGER
+);
+
 
 -- More INSERT
+
+INSERT INTO awards VALUES ('IIHS Safety Award','2015',49);
+INSERT INTO awards VALUES ('IIHS Safety Award','2015',50);
